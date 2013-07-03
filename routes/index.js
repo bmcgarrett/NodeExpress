e425998fce7bd406b91fb79c73c91209abc3fb00
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-03 14:28:32 -0400

    New Twitter Bootstrap style and fixed mongo db functionality

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@
  * GET home page.
  */
 
+
 exports.index = function(req, res){
   res.render('index', { title: 'Node Express App Home' });
 };
 
 exports.mongoTest = function(req, res){
-  res.render('mongo', { title: 'MongoDB',subHeading: 'Mongo'});
+  var mongo = require('mongoskin');
+  mongo.db('localhost:27017/myusert', {safe: true}).collection('myusers').find().toArray(function(err, result){
+        if (err) throw err;
+        res.render('mongo', { title: 'MongoDB',subHeading: 'Mongo',currentUsers: result });
+    });
+
+
 }
+
